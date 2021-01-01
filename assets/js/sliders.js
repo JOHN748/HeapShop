2555d2bce2e6d308458aee6aa0f25fefa2d59fce
JOHN748 <48284893+JOHN748@users.noreply.github.com>
2021-01-01 21:33:57 +0530

    Updated Featured Products Card
    Day 01-01-2001

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -60,23 +60,21 @@ $('.featured_product').slick({
 			{
 			breakpoint: 1024,
 			settings: {
-			slidesToShow: 5,
-			slidesToScroll: 2,
-			infinite: true,
-			dots: true
+			slidesToShow: 3,
+			slidesToScroll: 1
 			}
 			},
 			{
 			breakpoint: 600,
 			settings: {
-			slidesToShow: 4,
-			slidesToScroll: 2
+			slidesToShow: 2,
+			slidesToScroll: 1
 			}
 			},
 			{
 			breakpoint: 480,
 			settings: {
-			slidesToShow: 4,
+			slidesToShow: 1,
 			slidesToScroll: 1
 			}
 			}
--- includes/sections/todays-deal.php
+++ includes/sections/todays-deal.php
@@ -10,50 +10,29 @@
     <div class="card-body">
         <div class="row featured_product">
 
-            <div class="product-card">
-                <div class="product-img">                    
-                    <img src="https://themesbrand.com/skote/layouts/assets/images/product/img-1.png" class="product-img">
-                </div>
-            </div>
-            <div class="product-card">
-                <div class="product-img">                    
-                    <img src="https://themesbrand.com/skote/layouts/assets/images/product/img-4.png" class="product-img">
-                </div>
-            </div>
-            <div class="product-card">
-                <div class="product-img">                    
-                    <img src="https://themesbrand.com/skote/layouts/assets/images/product/img-2.png" class="product-img">
-                </div>
-            </div>
-            <div class="product-card">
-                <div class="product-img">                    
-                    <img src="https://themesbrand.com/skote/layouts/assets/images/product/img-3.png" class="product-img">
-                </div>
-            </div>
-            <div class="product-card">
-                <div class="product-img">                    
-                    <img src="https://themesbrand.com/skote/layouts/assets/images/product/img-5.png" class="product-img">
-                </div>
-            </div>
-            <div class="product-card">
-                <div class="product-img">                    
-                    <img src="https://themesbrand.com/skote/layouts/assets/images/product/img-6.png" class="product-img">
-                </div>
-            </div>
-
             <?php foreach ($featured_products as $key => $featured_product): ?>
-
-            <div class="product-card col-md-3">
-              <img  src="assets/images/products/<?php echo $featured_product['product_name']; ?>/<?php echo $featured_product['featured_image']; ?>" class="product-img">
-                <p class="card-text"><?php echo $featured_product['product_name']; ?></p>
+            <div class="fproduct">
+                <div class="fcard-img-top">
+                    <img src="assets/images/products/<?php echo $featured_product['product_name']; ?>/<?php echo $featured_product['featured_image']; ?>" class="fcard-img">
+                </div>
+                <div class="card_actions">
+                        <a href="" class="pico-left d-flex align-items-center">
+                            <i class="far fa-heart"></i>
+                        </a>
+                        <span class="price-bf"><?php echo $featured_product['mrp']; ?></span>
+                        <a href="" class="pico-right d-flex align-items-center">
+                            <i class="fas fa-cart-plus"></i>
+                        </a>
+                        <span class="price-af"><?php echo $featured_product['price']; ?></span>
+                </div>
+                <div class="product-body">
+                    <p class="fcard_title"><?php echo $featured_product['product_name']; ?></p>
+                </div>
             </div>
-
             <?php endforeach ?>
 
         </div>
-
     </div>
 
 </div>
-
 
